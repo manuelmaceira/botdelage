9e8cd1e1c6c8297db7fb39fd07cb6e3cdb508455
Manuel Maceira <manuelmaceira.m@gmail.com>
2022-04-18 20:43:27 -0300

    funciona

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,7 +1,12 @@
 import { Client } from "discord.js";
+import { registerListeners } from "./listeners";
+import "./bootstrap";
 
-console.log("Bot is starting...");
+console.log("Starting bot...");
 
-const client = new Client({ intents: [] });
+const client = new Client({
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"],
+});
 
-console.log(client);
+registerListeners(client);
+client.login(process.env.TOKEN);
